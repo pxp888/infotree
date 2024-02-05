d947fab3e1d731ac367a72c2f0ce5eb99d043ef1
pxp888 <pxperrine@gmail.com>
2024-02-05 03:23:36 +0100

    clean start

--- forest/admin.py
+++ forest/admin.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
-from .models import Tree, Branch, Node, Target, Member
+from .models import Node, Target
 
 # Register your models here.
-admin.site.register(Tree)
-admin.site.register(Branch)
 admin.site.register(Node)
 admin.site.register(Target)
-admin.site.register(Member)
+
--- forest/models.py
+++ forest/models.py
@@ -4,67 +4,29 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-class Tree(models.Model):
-    id = models.AutoField(primary_key=True)
-    topic = models.CharField(max_length=200)
-    created_on = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-
-    class Meta:
-        ordering = ['-created_on']
-
-    def __str__(self):
-        return self.topic
-
-
-class Branch(models.Model):
-    id = models.AutoField(primary_key=True)
-    tree = models.ForeignKey(Tree, on_delete=models.CASCADE, related_name='branches')
-    created_on = models.DateTimeField(auto_now_add=True)
-    subject = models.CharField(max_length=200)
-
-    class Meta:
-        ordering = ['-created_on']
-
-    def __str__(self):
-        return self.tree.topic + ' - ' + self.subject
-
-
-class Member(models.Model):
-    id = models.AutoField(primary_key=True)
-    tree = models.ForeignKey(Tree, on_delete=models.CASCADE, related_name='members')
-    branch = models.ForeignKey(Branch, on_delete=models.CASCADE, related_name='members')
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    created_on = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.tree.topic + ' - ' + self.branch.subject + ' - ' + self.user.username
 
 
 class Node(models.Model):
     id = models.AutoField(primary_key=True)
-    branch = models.ForeignKey(Branch, on_delete=models.CASCADE, related_name='nodes')
-    tree = models.ForeignKey(Tree, on_delete=models.CASCADE, related_name='nodes')
-    sender = models.ForeignKey(User, on_delete=models.CASCADE)
-    content = models.TextField(null=True, blank=True)
+    path = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
 
     class Meta:
         ordering = ['created_on']
 
     def __str__(self):
-        return self.branch.subject + ' - ' + self.sender.username + ' - ' + str(self.created_on)
+        return 'node : ' + self.author.username + '/' + str(self.path) + '/' + str(self.id)
 
 
 class Target(models.Model):
     id = models.AutoField(primary_key=True)
-    node = models.ForeignKey(Node, on_delete=models.CASCADE, related_name='targets')
-    target = models.ForeignKey(User, on_delete=models.CASCADE, related_name='targets')
-    branch = models.ForeignKey(Branch, on_delete=models.CASCADE, related_name='targets')
-    tree = models.ForeignKey(Tree, on_delete=models.CASCADE, related_name='targets')
-    notified = models.BooleanField(default=False)
+    node = models.ForeignKey(Node, on_delete=models.CASCADE)
+    target = models.ForeignKey(User, on_delete=models.CASCADE)
     read = models.BooleanField(default=False)
 
-    def __str__(self):
-        return self.target.username + ' - ' + str(self.node.id)
 
+    def __str__(self):
+        return 'path : ' + self.target.username + '/' + str(self.node.path) + '/' + str(self.node.id)
+    
